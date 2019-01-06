5360796d55a6147bc0f56ad16b49c5db2e0cf41c
Luke Rogerson <lukerogerson@aol.com>
2019-01-06 18:56:00 +0000

    Add component to prompt user to sign in first before being able to access site features

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4762d5a6503da313c27a";
+/******/ 	var hotCurrentHash = "c707623c2ccff9e7fb70";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
