8199fb822eab727d1c14947cc235f4bb5df2d91c
Luke Rogerson <lukerogerson@aol.com>
2019-01-09 12:41:30 +0000

    Commit RemoveFromCart component to use in cart view

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "59cfd73fee420664ac24";
+/******/ 	var hotCurrentHash = "b23868a2049879ecbc04";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
