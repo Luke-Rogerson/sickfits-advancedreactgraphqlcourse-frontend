861a15877f37f15d1ee0bb0bc399845e3c8e4ce1
Luke Rogerson <lukerogerson@aol.com>
2019-01-09 13:09:44 +0000

    Add caching and optimistic response functionality to automatically refresh deleted items in cart

--- components/RemoveFromCart.js
+++ components/RemoveFromCart.js
@@ -27,11 +27,31 @@ class RemoveFromCart extends React.Component {
   static propTypes = {
     id: PropTypes.string.isRequired
   };
+  // This gets called as soon as we get a response back from the server
+  // after the mutation has been performed.
+  update = (cache, payload) => {
+    // Read the cache
+    const data = cache.readQuery({ query: CURRENT_USER_QUERY });
+    // Remove that item from the cart
+    const cartItemId = payload.data.removeFromCart.id; // data is what we get back from the server, inside that we have a `removeFromCart` mutation, inside that is the ID
+    data.me.cart = data.me.cart.filter(cartItem => cartItem.id !== cartItemId);
+    // Write back to the cache
+    cache.writeQuery({ query: CURRENT_USER_QUERY, data });
+  };
   render() {
     return (
       <Mutation
         mutation={REMOVE_FROM_CART_MUTATION}
         variables={{ id: this.props.id }}
+        update={this.update} // If just a single update function, can call it this
+        optimisticResponse={{
+          // What you THINK the server will respond with
+          __typename: 'Mutation', // Needs a `typename`
+          removeFromCart: {
+            __typename: 'CartItem', //what we're returning
+            id: this.props.id
+          }
+        }}
       >
         {(removeFromCart, { loading, error }) => (
           <BigButton
